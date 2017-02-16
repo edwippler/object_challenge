e35089f0233d7cf7de9b9d47cb887b6fd3961060
Edmund Wippler <edmund@Edmunds-MacBook-Pro.local>
2017-02-15 20:30:51 -0600

    added constructor and pushed new instances of objects to array

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 var locations = []; // this array will hold your objects
 
 // object constructor
-
+function Region(state, revenue, locationCount) {
+  this.state = state;
+  this.revenue = revenue;
+  this.locationCount = locationCount;
+  this.averageRevenue = function() {
+    return this.revenue / this.locationCount;
+  }
+}
 // object instances
+var minnesota = new Region('MN', 300, 3);
+var illinois = new Region('IL', 5000, 12);
+var nevada = new Region('NV', 25000, 1);
+var ohio = new Region('OH', 700, 2);
 
 // push object instances to locations array
+locations.push(minnesota);
+locations.push(illinois);
+locations.push(nevada);
+locations.push(ohio);
 
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
